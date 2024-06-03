6bcdd3b39849dd5c30d4e5517cbb9323398289a2
unknown <giacomocappellotto@gmail.com>
2024-06-03 12:57:54 +0200

    not working

--- tccimetta/js/script.js
+++ tccimetta/js/script.js
@@ -1,8 +1,8 @@
-$(function () {
+$function () {
 	$("#navbarToggle").blur(function (event) {
 		var screenWidth = window.innerWidth;
 		if (screenWidth < 992){
 			$("#navbarSupportedContent").collapse('hide');
 		}
 	});
-});
+};
