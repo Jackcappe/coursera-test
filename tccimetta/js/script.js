d79645efa9fde674ffd77751d340b911f07a108b
unknown <giacomocappellotto@gmail.com>
2024-06-03 13:00:00 +0200

    shise

--- tccimetta/js/script.js
+++
@@ -1,8 +0,0 @@
-$function () {
-	$("#navbarToggle").blur(function (event) {
-		var screenWidth = window.innerWidth;
-		if (screenWidth < 992){
-			$("#navbarSupportedContent").collapse('hide');
-		}
-	});
-};
